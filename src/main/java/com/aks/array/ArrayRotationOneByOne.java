39118bc93092d6a19609ab46fbbcfc37c037140e
AnujKumarSoni <anujkumarsoni1993@gmail.com>
2019-07-12 21:42:01 +0530

    package structure changed

--- src/main/java/com/aks/array/ArrayRotationOneByOne.java
+++ src/main/java/com/aks/array/ArrayRotationOneByOne.java
@@ -1,10 +1,11 @@
+package com.aks.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class ArrayRotation {
+public class ArrayRotationOneByOne {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
